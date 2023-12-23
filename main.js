15b210eb449aff9080e1e6b166e65fb113ee719f
Twigbang510 <truong.nguyen5102002@gmail.com>
2023-12-23 11:26:06 +0700

    fix: ground

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import * as THREE from 'three';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
 
-let scene, camera, renderer, skyboxGeo, skybox, controls,tree;
+let scene, camera, renderer, skyboxGeo, skybox, controls, tree;
 
 function init() {
     scene = new THREE.Scene();
@@ -12,6 +12,18 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    const color = 0xFFFFFF;
+    const intensity = 1;
+
+    // Ambient Light
+    const ambientLight = new THREE.AmbientLight(color, intensity);
+    scene.add(ambientLight);
+
+    const light = new THREE.PointLight( 0xFFFFFF, 100,5, 20 );
+    light.position.set( 0, 10, 0 );
+    scene.add( light );
+
+
 
     createSkybox();
     createGround();
@@ -42,43 +54,64 @@ function createSkybox() {
 }
 
 function loadSnowTextures() {
-    const loader = new THREE.TextureLoader();
-    return {
-        albedoTexture: loader.load('textures/Snow_Albedo.jpg'),
-        ambientTexture: loader.load('textures/Snow_AmbientOcclusion.jpg'),
-        displacementTexture: loader.load('textures/Snow_Displacement.jpg'),
-        normalTexture: loader.load('textures/Snow_Normal.jpg'),
-        roughnessTexture: loader.load('textures/Snow_Roughness.jpg'),
-    };
+    try {
+        const loader = new THREE.TextureLoader();
+        return {
+            albedoTexture: loader.load('textures/Snow_Albedo.jpg'),
+            ambientTexture: loader.load('textures/Snow_AmbientOcclusion.jpg'),
+            displacementTexture: loader.load('textures/Snow_Displacement.jpg'),
+            normalTexture: loader.load('textures/Snow_Normal.jpg'),
+            roughnessTexture: loader.load('textures/Snow_Roughness.jpg'),
+        };
+    } catch (error) {
+        console.error('Error loading snow textures:', error);
+        throw error; // Re-throw the error to indicate failure
+    }
 }
 
 function createGround() {
-    const { albedoTexture, ambientTexture, displacementTexture, normalTexture, roughnessTexture } = loadSnowTextures();
-    const groundGeometry = new THREE.PlaneGeometry(100, 100);
-    const groundMaterial = new THREE.MeshStandardMaterial({
-        map: albedoTexture,
-        aoMap: ambientTexture,
-        displacementMap: displacementTexture,
-        normalMap: normalTexture,
-        roughnessMap: roughnessTexture,
-        displacementScale: 1,
-        color: [255, 255, 255],
-    });
+    try {
+        const { albedoTexture, ambientTexture, displacementTexture, normalTexture, roughnessTexture } = loadSnowTextures();
 
-    const ground = new THREE.Mesh(groundGeometry, groundMaterial);
-    ground.rotation.x = -Math.PI / 2;
-    ground.position.y = -0.75;
-    scene.add(ground);
+        const groundGeometry = new THREE.PlaneGeometry(20, 20);
+
+        const groundMaterial = new THREE.MeshStandardMaterial({
+            map: albedoTexture,
+            aoMap: ambientTexture,
+            displacementMap: displacementTexture,
+            normalMap: normalTexture,
+            roughnessMap: roughnessTexture,
+            displacementScale: 0.1,
+        });
+        const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+        ground.rotation.x = -Math.PI / 2;
+        ground.position.y = -0.75;
+
+        scene.add(ground);
+    } catch (error) {
+        console.error('Error creating ground:', error);
+    }
 }
+
 function loadChristmasTree() {
     const loader = new GLTFLoader();
-    loader.load('texture/christmas_tree_2.glb', (gltf) => {
+    loader.load('textures/christmas_tree_2.glb', (gltf) => {
         tree = gltf.scene;
-        tree.scale.set(1, 1, 1); // Adjust the scale as needed
-        tree.position.set(0, 0, 0); // Adjust the position as needed
+
+        // Traverse to set material properties
+        tree.traverse((child) => {
+            if (child.isMesh) {
+                // Set material side to double-sided
+                child.material.side = THREE.DoubleSide;
+            }
+        });
+
+        tree.scale.set(0.5, 0.5, 0.5); // Adjust the scale as needed
+        tree.position.set(0, -0.75, 0); // Adjust the position as needed
         scene.add(tree);
     });
 }
+
 function animate() {
     controls.update();
     renderer.render(scene, camera);
